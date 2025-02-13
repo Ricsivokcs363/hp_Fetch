db58d03b29dec88cb54a58c931df9ea35196e383
Ricsivokcs <kiss.richard.robert.363@dszcbaross.edu.hu>
2025-02-13 11:03:15 +0100

    fetch - response

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded',  fetchCharacters());
 
 async function fetchCharacters() {
     try {
-        const response = await fetch('https://raw.githubusercontent.com/Laboratoria/LIM011-data-lovers/master/src/data/potter/potter.json');
+        const response = await fetch('https://raw.githubusercontent.com/Laboratoria/LIM011-data-lovers/master/src/data/potter/potter.json')
         console.log(response);
     } catch (error) {
         
